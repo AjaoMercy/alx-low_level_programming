6b9058b369a0db5bbdef50b247c19eb0625d75f1
AjaoMercy <mercyajao16@gmail.com>
2023-03-17 08:22:25 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - known as the main function
+ *
+ * Return: should be one in this task
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (1);
+}
