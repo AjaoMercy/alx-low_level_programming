056fb45f6dbccda5439e64937d0660c47e667547
AjaoMercy <mercyajao16@gmail.com>
2023-03-19 13:59:44 -0700

    task 5 - Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: replies the main function
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
