6c5810e02c195de6b7df34e86bc8182653f3a16f
AjaoMercy <mercyajao16@gmail.com>
2023-04-10 08:39:42 -0700

    task 0-It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - the main function
+*
+*@argc: argument count
+*
+*@argv: argument vector
+*
+*Return: usually zero
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
