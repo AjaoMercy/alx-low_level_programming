440bbb59a885a195b0b82f33050e864061878b02
AjaoMercy <mercyajao16@gmail.com>
2023-04-17 08:21:32 -0700

    Task 0: Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - has elements that descibes a dog
+*
+*@name: dog's name
+*
+*@age: dog's age
+*
+*@owner: dog's owner
+*
+*Description: this is a structure called 'dog'
+*and it contains details about a dog like it's name, age and owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
